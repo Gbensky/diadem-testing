700b051debe1f1960b091688534c66a0102d04e6
Gbenro Oni <gbensky92@gmail.com>
2023-08-07 04:15:36 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@
 const config = {
   coverageThreshold: {
     global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
     }
   },
 };
